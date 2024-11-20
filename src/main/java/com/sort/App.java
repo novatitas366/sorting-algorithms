5021dd9d24dfa3038d66404f322d59a8c8326b3e
novatitas366 <novatitas366@gmail.com>
2024-11-20 15:28:01 +0200

    created algorithm to generate a random array of size 100000

--- src/main/java/com/sort/App.java
+++ src/main/java/com/sort/App.java
@@ -1,5 +1,8 @@
 package com.sort;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello World");
+        int n = 100000;
+        ArrayList <Integer> arr =new ArrayList<>();
+        Random rand = new Random();
+        
+        for(int i = 0; i<n; i++){
+            arr.add(i, rand.nextInt(0,10000));
+        }
+        
+        Quicksort.quickSort(arr, n);
     }
 }
--- src/main/java/com/sort/Quicksort.java
+++ src/main/java/com/sort/Quicksort.java
@@ -1,7 +1,11 @@
 package com.sort;
 
+
+import java.util.ArrayList;
 public class Quicksort {
-    public static void quickSort(){
-        System.out.println("sort veikia");
+    public static void quickSort(ArrayList<Integer> arr, int n){
+        for(int i = 0; i<n; i++){
+            System.out.println(arr.get(i));
+        }
     }
 }
