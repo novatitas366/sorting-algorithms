33d415c679b2901c358f07d1587ca0c3fe5cd8f5
novatitas366 <novatitas366@gmail.com>
2024-11-21 19:45:03 +0200

    new App.java

--- src/main/java/com/sort/App.java
+++ src/main/java/com/sort/App.java
@@ -3,24 +3,21 @@ package com.sort;
 import java.util.ArrayList;
 import java.util.Random;
 
-/**
- * Hello world!
- *
- */
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        int n = 100000;
+        int n = 5;
         ArrayList <Integer> arr =new ArrayList<>();
         Random rand = new Random();
         
         for(int i = 0; i<n; i++){
-            arr.add(i, rand.nextInt(0,10000));
+            arr.add(i, rand.nextInt(n+5));
         }
-
-
-
-        Quicksort.quickSort(arr, n);
+        System.out.println(arr);
+        Quicksort.quickSort(arr, 0, n-1);
+        System.out.println(arr);
     }
 }
