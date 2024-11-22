163875196f3ae26d8220e5d16af56685141c2da0
novatitas366 <novatitas366@gmail.com>
2024-11-22 19:54:53 +0200

    added Multithreaded functionality to Quicksort

--- src/main/java/com/sort/Quicksort.java
+++ src/main/java/com/sort/Quicksort.java
@@ -1,19 +1,38 @@
 package com.sort;
-
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.ArrayList;
 
 public class Quicksort {
+
+    private final static AtomicInteger activeTaskCount = new AtomicInteger(0);
+
     public static void quickSort(ArrayList<Integer> arr, int start, int end){
-        
+        ExecutorService executor = Executors.newFixedThreadPool(2);
         if(start>=end){
             return;
         }
         int pivot = m_findPivotAndSort(arr, start, end);
+        activeTaskCount.incrementAndGet();
+
+        executor.submit(() -> {
+            quickSort(arr, start, pivot-1);
+            if (activeTaskCount.decrementAndGet() == 0) {
+                executor.shutdown();
+            }
+        });
+
+            
+        activeTaskCount.incrementAndGet();
+        executor.submit(() -> {
+            quickSort(arr, pivot+1, end);
+            if(activeTaskCount.decrementAndGet() == 0){
+                executor.shutdown();
+            }
         
-        quickSort(arr, start, pivot-1);
-        quickSort(arr, pivot+1, end);
-        
+        });
+        executor.shutdown();
     }
 
     private static int m_findPivotAndSort(ArrayList<Integer> arr, int start, int end){
