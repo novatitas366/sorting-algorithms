b68aa6804e4f5cd1706db05eb477a6d357169996
ArnCode <arnoldasstasiunas@gmail.com>
2024-11-25 22:07:51 +0200

    Heap sort finished with time calc

--- src/main/java/com/sort/App.java
+++ src/main/java/com/sort/App.java
@@ -10,17 +10,20 @@ public class App
     
     public static void main( String[] args )
     {
-        int n = 20;
+        int n = 100;
         ArrayList <Integer> arr =new ArrayList<>();
         Random rand = new Random();
         
         for(int i = 0; i<n; i++){
             arr.add(i, rand.nextInt(n+5));
         }
-        System.out.println(arr);
+        //System.out.println(arr);
         //Quicksort.QuickSortWithTime(arr);
-        //Heap.HeapSortWithTime(arr);
-        Shell.ShellSortWithTime(arr);
-        System.out.println(arr);
+        //System.out.println(arr);
+
+        //Heap.printArray(arr);
+        Heap.heapSort(arr);
+        //Heap.printArray(arr);
+        Heap.HeapSortWithTime(arr);
     }
 }
