9f5ea4d4d71a1135721db7f1c60da91247b77602
novatitas366 <novatitas366@gmail.com>
2024-11-26 17:26:33 +0200

    added a function to make a txt file to store arrays and a function to read that file

--- src/main/java/com/sort/App.java
+++ src/main/java/com/sort/App.java
@@ -2,28 +2,58 @@ package com.sort;
 
 import java.util.ArrayList;
 import java.util.Random;
-
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 
 public class App 
 {
-    
-    public static void main( String[] args )
-    {
-        int n = 1000000;
+    public static void createFileWithNames(){
+        int n = 20;
         ArrayList <Integer> arr =new ArrayList<>();
         Random rand = new Random();
         
         for(int i = 0; i<n; i++){
             arr.add(i, rand.nextInt(n+5));
         }
-        //System.out.println(arr);
-        //Quicksort.QuickSortWithTime(arr);
-        //System.out.println(arr);
+        try{
+        FileWriter fw = new FileWriter("arr.txt");
+        for(int i = 0; i<arr.size(); i++){
+            fw.write(arr.get(i).toString());
+            fw.write(System.lineSeparator());
+        }
+        fw.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 
-        //Heap.printArray(arr);
-        Heap.heapSort(arr);
-        //Heap.printArray(arr);
-        Heap.HeapSortWithTime(arr);
+    public static void main( String[] args )
+    {
+        createFileWithNames();
+
+        ArrayList <Integer> arr = new ArrayList<>();
+
+        File file = new File("arr.txt");
+        try{
+            Scanner sc = new Scanner(file);
+            while(sc.hasNextInt()){
+                arr.add(sc.nextInt());
+            }
+            sc.close();
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        
+        
+        System.out.println(arr);
+        //Quicksort.QuickSortWithTime(arr);
+        Shell.ShellSortWithTime(arr);
+        //System.out.println(arr);
+        //Heap.HeapSortWithTime(arr);
+        System.out.println(arr);
     }
 }
